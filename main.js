96c2b7da35765afaca1c03041cf36c3fca2db551
Simcha <siiiimcha@gmail.com>
2020-11-11 21:17:55 +0200

    incomplete

---
+++ main.js
@@ -0,0 +1,65 @@
+// ====================== Exercise_1 ============================
+
+// const playingField = document.getElementById("playing-field")
+// console.log(playingField)
+
+
+// const blockUp = document.getElementById("block")
+
+// const up = function () {
+//     let top = parseInt(block.style.bottom) || 0
+//     top += 15
+//     block.style.bottom = top + "px"
+// }
+
+// const blockLeft = document.getElementById("block")
+
+// const left = function () {
+//     let left = parseInt(block.style.right) || 0
+//     left += 15
+//     block.style.right = left + "px"
+// }
+
+// const blockRight= document.getElementById("block")
+
+// const right = function () {
+//     let right = parseInt(block.style.left) || 0
+//     right += 15
+//     block.style.left = right + "px"
+// }
+
+
+// const blockDown = document.getElementById("block")
+
+// const down = function () {
+//     let down = parseInt(block.style.top) || 0
+//     down += 15
+//     block.style.top = down + "px"
+// }
+
+// ====================== Exercise_2 ============================
+
+
+const reservations = {
+    Bob: { claimed: false },
+    Ted: { claimed: true }
+}
+
+const checkReservation = function () {
+    const name = document.getElementById("name").value
+    if (reservations.Bob.claimed){
+        console.log()
+    }
+}
+
+// ====================== Exercise_3 ============================
+
+
+const box = document.getElementById("container")
+
+
+
+
+
+
+
